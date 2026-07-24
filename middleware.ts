a5efaf26ffab55d023fbcb19329108459b8585bf
hchansl02 <sunchansol02@gmail.com>
2026-07-25 03:29:16 +0900

    middleware 쿠키 타입 에러 수정

--- middleware.ts
+++ middleware.ts
@@ -14,15 +14,13 @@ export async function middleware(request: NextRequest) {
         getAll() {
           return request.cookies.getAll()
         },
-        setAll(cookiesToSet) {
-          cookiesToSet.forEach(({ name, value, options }) => request.cookies.set(name, value))
-          supabaseResponse = NextResponse.next({
-            request,
-          })
-          cookiesToSet.forEach(({ name, value, options }) =>
-            supabaseResponse.cookies.set(name, value, options)
-          )
-        },
+        setAll(cookiesToSet: any) {
+            cookiesToSet.forEach(({ name, value, options }: { name: string; value: string; options: any }) => request.cookies.set(name, value))
+            supabaseResponse = NextResponse.next({
+              request,
+            })
+            cookiesToSet.forEach(({ name, value, options }: { name: string; value: string; options: any }) => supabaseResponse.cookies.set(name, value, options))
+          },
       },
     }
   )
